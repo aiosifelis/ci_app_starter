18c7bbee2e5bec790d9e5b1191cd7d28016a88c7
Andreas Iosifelis <aiosifelisl1@loyalty.com>
2020-11-04 09:05:03 -0500

    sample

---
+++ app_settings_sample.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+    'name' => 'app',
+    'version' => 1,
+    'db_dsn' => '',
+    'db_user' => '',
+    'db_pass' => '',
+    'db_char_set' => '',
+    'db_collat' => '',
+    'timezone' => '',
+    'base_url' => '',
+    'default_language' => '',
+    'encryption_key' => '',
+    'cookie_domain' => '',
+    'cookie_secure' => '',
+    'session_cookie_name' => '',
+    'session_life' => '',
+    'session_table_name' => '',
+    'sendgrid_api_key' => ''
+);
